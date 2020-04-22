00f085d1830e6b63df5f7a8a8e837abe88eea55d
Abinaya Raja <abinayaraja1411@gmail.com>
2020-04-22 19:44:36 -0400

    Initial setup MVC

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace GroceryAtYourDoor
 {
@@ -15,6 +16,7 @@ namespace GroceryAtYourDoor
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,7 +26,7 @@ namespace GroceryAtYourDoor
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseMvcWithDefaultRoute();
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
