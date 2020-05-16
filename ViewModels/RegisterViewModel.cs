5fb77ce5b132b6f4c38a6936ff9dce293820c460
Abinaya Raja <abinayaraja1411@gmail.com>
2020-05-16 19:50:18 -0400

    responsive home - register, login, highlights

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroceryAtYourDoor.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
